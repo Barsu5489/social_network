130cd363d3d2c639f50bc5920e2a45773051132d
ebarsula <ebarsulai@gmail.com>
2025-05-17 06:44:39 +0300

    feat(delet): delete posts created by user

--- backend/pkg/handlers/post_handler.go
+++ backend/pkg/handlers/post_handler.go
@@ -9,60 +9,77 @@ import (
 	"time"
 
 	"social-nework/pkg/models"
+
+	"github.com/gorilla/mux"
 )
 
-
 func NewPost(db *sql.DB) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request){
+	return func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodPost {
+			http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+			return
+		}
+		userID, ok := r.Context().Value("user_id").(string)
 
-	
-	if r.Method != http.MethodPost {
-		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
-		return
+		if !ok {
+			http.Error(w, "Unauthorized", http.StatusUnauthorized)
+			return
+		}
+		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+		defer cancel()
+		var req models.Post
+		if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+			http.Error(w, "Invalid request body", http.StatusBadRequest)
+			return
+		}
+		_, err := models.CreatePost(db, ctx, userID, req.Content, req.Privacy, req.GroupID)
+		if err != nil {
+			http.Error(w, "Error creating Post", http.StatusBadRequest)
+			return
+		}
+		w.WriteHeader(http.StatusCreated)
+		json.NewEncoder(w).Encode(map[string]string{"message": "Post Created successfully"})
 	}
-	userID, ok := r.Context().Value("user_id").(string)
-
-	if !ok {
-		http.Error(w, "Unauthorized", http.StatusUnauthorized)
-		return
-	}
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-	defer cancel()
-	var req models.Post
-	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
-		http.Error(w, "Invalid request body", http.StatusBadRequest)
-		return
-	}
-	_, err := models.CreatePost(db, ctx, userID, req.Content, req.Privacy, req.GroupID)
-	if err != nil {
-		http.Error(w, "Error creating Post", http.StatusBadRequest)
-		return
-	}
-	w.WriteHeader(http.StatusCreated)
-	json.NewEncoder(w).Encode(map[string]string{"message": "Post Created successfully"})
-}
 }
 
-func FollowingPosts(db *sql.DB) http.HandlerFunc{
-	return func(w http.ResponseWriter, r *http.Request){
-		if r.Method != http.MethodGet{
+func FollowingPosts(db *sql.DB) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodGet {
 
-		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
-		return
+			http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+			return
 		}
 		userID := r.Context().Value("user_id").(string)
 
 		posts, err := models.GetFollowingPosts(db, userID)
-
-		if err != nil{
+		if err != nil {
 			fmt.Println(err)
 			http.Error(w, "Error getting Post", http.StatusMethodNotAllowed)
 		}
-		
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
 
-	// Encode posts as JSON
-	json.NewEncoder(w).Encode(posts)
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+
+		// Encode posts as JSON
+		json.NewEncoder(w).Encode(posts)
+	}
+}
+
+func DeletPost(db *sql.DB) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		userID := r.Context().Value("user_id").(string)
+		vars := mux.Vars(r)
+		postID := vars["post_id"]
+
+		err := models.DeletePost(db, postID, userID)
+	
+		
+		if err != nil {
+			http.Error(w, "Error deleting post: "+err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		w.WriteHeader(http.StatusOK)
+		json.NewEncoder(w).Encode(map[string]string{"message": "Post deleted successfully"})
 	}
 }
--- backend/pkg/models/posts.go
+++ backend/pkg/models/posts.go
@@ -9,8 +9,6 @@ import (
 	"github.com/google/uuid"
 )
 
-
-
 func CreatePost(db *sql.DB, ctx context.Context, userID, content, privacy string, groupID *string) (string, error) {
 	if content == "" {
 		return "", errors.New("content cannot be empty")
@@ -24,20 +22,19 @@ func CreatePost(db *sql.DB, ctx context.Context, userID, content, privacy string
 	stm := `INSERT INTO posts (id, user_id, group_id, content, privacy, created_at, updated_at)
 	VALUES(?,?,?,?,?,?,?)
 	`
-	result, err := db.ExecContext(ctx, stm, id,userID,  groupID, content, privacy,now, now)
-
-	if err != nil{
+	result, err := db.ExecContext(ctx, stm, id, userID, groupID, content, privacy, now, now)
+	if err != nil {
 		return "", err
 	}
 	rowsAffected, _ := result.RowsAffected()
-if rowsAffected == 0 {
-    return "", errors.New("post insert failed")
-}
+	if rowsAffected == 0 {
+		return "", errors.New("post insert failed")
+	}
 	return id, nil
 }
-// GetFeed retrieves posts from users the given user follows
-func GetFollowingPosts(db *sql.DB, userID string)([]Post, error){
 
+// GetFeed retrieves posts from users the given user follows
+func GetFollowingPosts(db *sql.DB, userID string) ([]Post, error) {
 	stm := `
 	SELECT p.id, p.user_id, p.content, p.created_at
 	FROM posts p
@@ -46,25 +43,43 @@ func GetFollowingPosts(db *sql.DB, userID string)([]Post, error){
 	ORDER BY p.created_at DESC;
 	`
 	rows, err := db.Query(stm, userID)
-
-	if err != nil{
+	if err != nil {
 		return []Post{}, err
 	}
 	defer rows.Close()
 
 	var posts []Post
 
-	for rows.Next(){
+	for rows.Next() {
 		var post Post
-		 err := rows.Scan(&post.ID,&post.UserID, &post.Content,&post.CreatedAt)
-
-		 if err!= nil{
+		err := rows.Scan(&post.ID, &post.UserID, &post.Content, &post.CreatedAt)
+		if err != nil {
 			return nil, err
-		 }
-		 posts = append(posts, post)
+		}
+		posts = append(posts, post)
 	}
-if posts == nil{
-	return []Post{}, nil
+	if posts == nil {
+		return []Post{}, nil
+	}
+	return posts, nil
 }
-return posts,nil
+
+func DeletePost(db *sql.DB, postID, userID string) error {
+	stmt := `
+		DELETE FROM posts
+		WHERE id = ? AND user_id = ?;
+`
+	result, err := db.Exec(stmt, postID, userID)
+	if err != nil {
+		return err
+	}
+	rows, err := result.RowsAffected()
+	if err != nil {
+		return err
+	}
+	if rows == 0 {
+		return errors.New("post not found or not owned by user")
+	}
+
+	return nil
 }
