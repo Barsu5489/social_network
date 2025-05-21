33ed5fd56078ac5caebb041031603ef6569d0683
ebarsula <ebarsulai@gmail.com>
2025-05-22 01:12:39 +0300

    fix: change from password_hash to password

--- backend/pkg/models/models.go
+++ backend/pkg/models/models.go
@@ -3,7 +3,7 @@ package models
 type User struct {
 	ID           string `json:"id"`            
 	Email        string `json:"email"`          
-	PasswordHash string `json:"password_hash"`  
+	PasswordHash string `json:"password"`  
 	FirstName    string `json:"first_name"`    
 	LastName     string `json:"last_name"`     
 	Nickname     string `json:"nickname"`       
