fd0aad956da36bfb77ff734e72bc13c8b7e586ed
ebarsula <ebarsulai@gmail.com>
2025-05-22 03:02:51 +0300

    fix: Modify likes table and table migrate

--- backend/pkg/db/migrations/sqlite/000029_create_likes_table.up.sql
+++ backend/pkg/db/migrations/sqlite/000029_create_likes_table.up.sql
@@ -1,9 +1,9 @@
 CREATE TABLE likes (
     id UUID PRIMARY KEY,
     user_id UUID REFERENCES users(id),
-    likeable_type ENUM('post', 'comment'),
+     likeable_type TEXT CHECK (likeable_type IN ('post', 'comment')),
     likeable_id UUID,
-    created_at TIMESTAMP,
-    deleted_at TIMESTAMP,
+    created_at INTEGER,
+    deleted_at INTEGER,
     CONSTRAINT likes_user_content_unique UNIQUE(user_id, likeable_type, likeable_id)
 );
